3c408a55edbe067d54a9fd44217a5f6a5b921cdf
Dan Barela <daniel.barela@gmail.com>
2017-12-13 12:36:26 -0700

    adding alert

--- MapCalloutTest/ViewController.m
+++ MapCalloutTest/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
@@ -16,14 +19,58 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.mapView.delegate = self;
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    NSMutableArray *annotations = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < 100; i++) {
+        double x = -104.0 + arc4random_uniform(10);
+        double y = 39.5 + arc4random_uniform(10);
+        
+        MKPointAnnotation* pointAnnotation = [[MKPointAnnotation alloc] init];
+        pointAnnotation.coordinate = CLLocationCoordinate2DMake(y, x);
+        pointAnnotation.title = @"Title";
+        [annotations addObject:pointAnnotation];
+    }
+    
+    [self.mapView addAnnotations:annotations];
 }
 
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation {
+    if ([annotation isKindOfClass:[MKUserLocation class]]) {
+        return nil;
+    }
+    
+    static NSString* ShopAnnotationIdentifier = @"test";
+    MKPinAnnotationView *pinView = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:ShopAnnotationIdentifier];
+    if (!pinView) {
+        pinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:ShopAnnotationIdentifier];
+        pinView.canShowCallout = YES;
+        UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeInfoLight];
+        rightButton.tintColor = [UIColor colorWithRed:17.0/255.0 green:84.0/255.0 blue:164.0/255.0 alpha:1.0];
+        pinView.rightCalloutAccessoryView = rightButton;
+    }
+    return pinView;
+}
+
+- (void) mapView:(MKMapView *) mapView annotationView:(MKAnnotationView *) view calloutAccessoryControlTapped:(UIControl *) control {
+    NSLog(@"Callout accessory tapped");
+
+    UIAlertController * alert = [UIAlertController
+                                 alertControllerWithTitle:@"Tapped"
+                                 message:@"Map Pin tapped"
+                                 preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* okButton = [UIAlertAction
+                               actionWithTitle:@"OK"
+                               style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction * action) {
+                               }];
+    [alert addAction:okButton];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 @end
